af1257595ac643d32177c8c42dbee921bdc97e11
swebgit <samweber609@gmail.com>
2018-11-15 18:46:36 -0600

    Setting up app manifest for android

--- MobileApp/Tdp.MobileApp.Forms.Android/MainActivity.cs
+++ MobileApp/Tdp.MobileApp.Forms.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Tdp.MobileApp.Forms.Droid
 {
-    [Activity(Label = "Tdp.MobileApp.Forms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Dog Park", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
